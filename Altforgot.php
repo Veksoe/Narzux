fbee411f590729089c5e0d0afe9eb3508db27f70
Lasse Rasmussen <freedomforce360@gmail.com>
2016-05-11 21:40:01 +0200

    Forgot rework
    Rework the forgot system, due to the site we are using can't send
    Emails. 
    Added to new values: Rank(For a rank system) & Question(For the user
    Question).

--- Altforgot.php
+++ Altforgot.php
@@ -3,7 +3,7 @@ include 'php/dba.php';
 require "php/classes/ED.class.php";
 
 $error = "";
-$done = false;
+
 if (isset ( $_POST ["username"] )) {
 	
 	$myusername = DB::esc ( $_POST ["username"] );
@@ -14,19 +14,22 @@ if (isset ( $_POST ["username"] )) {
 	$count = mysqli_num_rows ( $q );
 	if ($count > 0) {
 		
-		$message = "\nNARZUX web team ";
-		$message .= "\nhttp://narzux.weebly.com/ ";
+		$to = $myusername;
+		$subject = "Narzux: Glemt kode";
 		
-		$header = "Hej $myusername,";
-		$header .= " <br>Du har bedt om din kode til Narzux!\n";
-		$header .= "<br>Her er din kode:<b> ";
+		$message = "NARZUX web team ";
+		$message .= "http://narzux.weebly.com/ ";
+		
+		$header = "Hej, ";
+		$header .= $myuseremail;
+		$header .= " \nDu har bedt om din kode til Narzux!\n";
+		$header .= "Her står koden: ";
 		
 		while ( $row = $q->fetch_assoc () ) {
 			$header .= $decrypted_txt = ED::decrypt ( $row ["Password"] );
 		}
-		$done = true;
-		
-		// header ( "location:/dashboard/Annika/Narzux/index.php" );
+		$retval = mail ( $to, $subject, $message, $header );
+		header ( "location:/dashboard/Annika/Narzux/index.php" );
 	} else {
 		$error = "Unknown Email or Username, please try again or contact a admin.";
 	}
@@ -51,24 +54,14 @@ if (isset ( $_POST ["username"] )) {
 	<form action="" method="post">
 		<div class="main">
 			<div class="logo"></div>
-			<?php if($done == false){?>
 			<a class="text">Brugernavn</a> <input type="text" name="username"
 				id="username" placeholder="Brugernavn" class="input"
 				required="required"> <a class="text">Email</a><input type="email"
 				name="useremail" id="useremail" placeholder="Email" class="input"
-				required="required"> <a class="text">Hvad var yndlings dyr?</a> <input
-				name="userQuestion" id="userQuestion" placeholder="Svar"
-				class="input" required="required"><input type="submit"
-				class="sendButton"
+				required="required"> <input type="submit" class="sendButton"
 				onkeydown="if (event.keyCode == 13) { this.form.submit(); return false; }">
 			<div style="color: red; text-align: center;">
-				<?php
-				
-				echo $error;
-			} else {
-				echo "<center>", $header, "</b><br><br>", $message, "<br><input type='submit' value='Tilbage' </center>";
-			}
-			?>
+				<?php echo $error?>
 			</div>
 		</div>
 	</form>
--- register.php
+++ register.php
@@ -3,26 +3,23 @@ include 'php/dba.php';
 require "php/classes/ED.class.php";
 $error = "";
 if (isset ( $_POST ["username"] )) {
-	if ($_POST ["userpassword"] == $_POST ["userpassword2"]) {
-		$myusername = DB::esc ( $_POST ["username"] );
-		$sql = "SELECT id FROM members WHERE Username = '$myusername'";
-		$q = DB::query ( $sql );
-		$count = mysqli_num_rows ( $q );
-		echo $count;
-		if ($count < 1) {
-			
-			$encrypted_txt = DB::esc ( ED::encrypt ( $_POST ["userpassword"] ) );
-			$myuseremail = DB::esc ( $_POST ["useremail"] );
-			$date = date ( 'j/n-H:i' );
-			$sql2 = "INSERT INTO members (Username, Password, Email, Joindate, lastOnline) VALUES ('$myusername','$encrypted_txt','$myuseremail', '$date','$date')";
-			$q2 = DB::query ( $sql2 );
-			
-			header ( "location:/dashboard/Annika/Narzux/index.php" );
-		} else {
-			$error = "Error: Username: $myusername is already in use!";
-		}
+	$myusername = DB::esc ( $_POST ["username"] );
+	$sql = "SELECT id FROM members WHERE Username = '$myusername'";
+	$q = DB::query ( $sql );
+	$count = mysqli_num_rows ( $q );
+	echo $count;
+	if ($count < 1) {
+		
+		$encrypted_txt = DB::esc ( ED::encrypt ( $_POST ["userpassword"] ) );
+		$myuseremail = DB::esc ( $_POST ["useremail"] );
+		$myUserQustion = DB::esc ( $_POST ["userQuestion"] );
+		$date = date ( 'j/n-H:i' );
+		$sql2 = "INSERT INTO members (Username, Password, Email, Joindate, lastOnline, Rank, Question) VALUES ('$myusername','$encrypted_txt','$myuseremail', '$date','$date', 1,'$myUserQustion')";
+		$q2 = DB::query ( $sql2 );
+		
+		header ( "location:/dashboard/Annika/Narzux/index.php" );
 	} else {
-		$error = "Error: Password is not equal!";
+		$error = "Error: Username: $myusername is already in use!";
 	}
 }
 ?>
@@ -48,10 +45,10 @@ if (isset ( $_POST ["username"] )) {
 				name="useremail" id="useremail" placeholder="Email" class="input"
 				required="required"> <a class="text">Kode</a><input type="password"
 				name="userpassword" id="userpassword" placeholder="Kode"
-				class="input" required="required"> <a class="text">Gentag koden</a><input
-				type="password" placeholder="Kode" class="input" required="required"
-				name="userpassword2"> <input type="submit" class="sendButton"
-				style="margin-left: 5%;"
+				class="input" required="required"> <a class="text">Hvad var yndlings
+				dyr?</a><input type="text" placeholder="Svar" class="input"
+				required="required" id="userQuestion" name="userQuestion"> <input
+				type="submit" class="sendButton" style="margin-left: 5%;"
 				onkeydown="if (event.keyCode == 13) { this.form.submit(); return false; }">
 			<div style="color: red; text-align: center;">
 				<?php echo $error?>
